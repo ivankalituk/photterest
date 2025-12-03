26b8bb3f626dfe69a699905bfe2bd3b456a2b11a
ivankalituk <63204841+ivankalituk@users.noreply.github.com>
2025-12-04 00:32:02 +0200

    feat(routes): sample routing check

---
+++ src/app/post/page.tsx
@@ -0,0 +1,9 @@
+import { FC } from "react";
+
+const PostPage: FC = () =>{
+    return(
+        <div>POST</div>
+    )
+}
+
+export default PostPage
---
+++ src/widgets/header/ui/header.tsx
@@ -0,0 +1,11 @@
+'use client'
+
+import { FC } from "react"
+
+const Header: FC = () => {
+    return(
+        <header>Header</header>
+    )
+}
+
+export default Header
