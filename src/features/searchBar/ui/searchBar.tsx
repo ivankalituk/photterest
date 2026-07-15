7d5a7f997682ebd35f6f922afc700002f841763c
ivankalituk <63204841+ivankalituk@users.noreply.github.com>
2026-07-15 18:31:13 +0300

    feat[searchBar]: custom input and twMerge

--- src/features/searchBar/ui/searchBar.tsx
+++ src/features/searchBar/ui/searchBar.tsx
@@ -2,14 +2,34 @@ import SearchSVG from "@/shared/assets/controlledSVG/searchSVG";
 import { Button } from "@/shared/ui/button";
 import { FC } from "react";
 import PhotoSearch from "@/shared/assets/controlledSVG/photoSearch";
+import { Input } from "@/shared/ui/input";
 
 
 const SearchBar: FC = () => {
     return(
-        <div>
+        <div
+            className="
+                flex
+                bg-grey-light
+                flex-1
+                items-center
+                justify-between
+                gap-[10px]
+                h-[48px]
+                pl-[16px]
+                rounded-[12px]
+            "
+        >
             <SearchSVG />
-            
-            <input type="text" />
+
+            <Input 
+                className="
+                    flex-1
+                    bg-[green]
+                    h-[100%]
+                "
+                
+            />
 
             <Button>
                 <PhotoSearch />
---
+++ src/shared/ui/input/ui/input.tsx
@@ -0,0 +1,24 @@
+import { FC } from "react";
+import { twMerge } from 'tailwind-merge'
+
+interface Props {
+    className?: string
+    placeholder?: string
+}
+
+const Input: FC <Props> = ({className, placeholder}) => {
+    return(
+        <input 
+            type="text"
+            placeholder = {placeholder}
+            className={twMerge(`
+                bg-[red]
+                flex-1
+                outline-none
+                border-0
+            `, className)}
+        />
+    )
+}
+
+export default Input
