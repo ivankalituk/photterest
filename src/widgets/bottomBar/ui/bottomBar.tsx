e16ee9af07ff5568ad3d989422c08c6e4a347f3a
ivankalituk <63204841+ivankalituk@users.noreply.github.com>
2026-07-28 13:57:37 +0300

    feat[app]: server/client and next special issues fix

--- src/widgets/bottomBar/ui/bottomBar.tsx
+++ src/widgets/bottomBar/ui/bottomBar.tsx
@@ -1,19 +1,8 @@
-'use client'
 import { FC } from "react";
 import { bottomBarData, BottomBarItemType } from "../model/mockData";
 import BottomBarButton from "./components/bottomBarButton";
-import { useScrollDirection } from "@/shared/hooks/useScrollDirection";
 
 const BottomBar: FC = () => {
-
-    const direction = useScrollDirection();
-
-    const hidden = direction === 'down';
-
-    // пока не знаю нужно ли делать скрытие, вроде в пинтересте
-    // оно есть, а вроде и нет, там не понять, может зависит от каких-то настроек
-    // если нужно убрать, просто убираю хидден
-
     return(
         <nav
             className={`
@@ -27,15 +16,6 @@ const BottomBar: FC = () => {
                 bg-[white]
                 border-t
                 border-border
-                transition-all
-                duration-300
-
-                ${hidden 
-                    ? 
-                    "opacity-0 translate-y-full pointer-events-none"
-                    :
-                    "opacity-100 translate-y-0"
-                }
 
                 lg:hidden
             `}
--- src/widgets/bottomBar/ui/components/bottomBarButton.tsx
+++ src/widgets/bottomBar/ui/components/bottomBarButton.tsx
@@ -1,33 +1,59 @@
-'use client'
-import { Button } from "@/shared/ui/button";
+'use client';
+
 import { FC } from "react";
-import { BottomBarItemType } from "../../model/mockData";
 import { usePathname } from "next/navigation";
 
-interface Props{
-    item: BottomBarItemType
+import HomeSVG from "@/shared/assets/controlledSVG/homeSVG";
+import SearchSVG from "@/shared/assets/controlledSVG/searchSVG";
+import MessagesSVG from "@/shared/assets/controlledSVG/messagesSVG";
+import ProfileSVG from "@/shared/assets/controlledSVG/profileSVG";
+
+import { Button } from "@/shared/ui/button";
+import { BottomBarItemType } from "../../model/mockData";
+
+interface Props {
+    item: BottomBarItemType;
 }
 
-const BottomBarButton: FC <Props> = ({item}) => {
+const BottomBarButton: FC<Props> = ({ item }) => {
 
-    const location = usePathname()
+    const location = usePathname();
 
-    // active - #000000
-    // unactive - #62625b
+    const active = location === item.link;
+
+    const fill = active ? '#000000' : '#62625b';
 
 
-    return(
-        <Button 
-            as="LINK" 
+    const renderIcon = () => {
+        switch (item.id) {
+            case 'HOME':
+                return <HomeSVG active={active} fill={fill} />;
+
+            case 'SEARCH':
+                return <SearchSVG active={active} fill={fill} />;
+
+            case 'MESSAGES':
+                return <MessagesSVG active={active} fill={fill} />;
+
+            case 'PROFILE':
+                return <ProfileSVG active={active} fill={fill} />;
+
+            default:
+                return null;
+        }
+    };
+
+
+    return (
+        <Button
+            as="LINK"
             href={item.link}
-            className="
-                flex-1
-            "
+            className="flex-1"
             scaling
         >
-            <item.svg active={location === item.link} fill={location === item.link? '#000000' : '#62625b'}/>
+            {renderIcon()}
         </Button>
-    )
-}
+    );
+};
 
-export default BottomBarButton
+export default BottomBarButton;
