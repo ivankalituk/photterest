f8d06d093d540b0ec6195fb7aa999fb522504109
ivankalituk <63204841+ivankalituk@users.noreply.github.com>
2026-08-23 18:44:03 +0300

    feat[auth]: google auth

--- src/app/authPage/page.tsx
+++ src/app/authPage/page.tsx
@@ -1,19 +1,46 @@
-import { Button } from "@/shared/ui/button";
+'use client'
+
+import { GoogleLogin } from "@react-oauth/google";
 import { FC } from "react";
 
 const AuthPage: FC = () => {
-    return(
-        <Button
-            type="WHITE"
-            className="
-                bg-[green]
-                p-[8px]
-                text-[white]
-            "
-        >
-            Google auth
-        </Button>
-    )
+
+    const handleGoogleLogin = async (credential: string) => {
+        try {
+            const response = await fetch(
+                'http://localhost:5000/auth/google',
+                {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify({
+                        token: credential,
+                    }),
+                }
+            );
+
+            const data = await response.json();
+
+            console.log(data);
+
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
+    return (
+        <GoogleLogin
+            onSuccess={(response) => {
+                if (response.credential) {
+                    handleGoogleLogin(response.credential);
+                }
+            }}
+            onError={() => {
+                console.log('Google auth failed');
+            }}
+        />
+    );
 }
 
 export default AuthPage
